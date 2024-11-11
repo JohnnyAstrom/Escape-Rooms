35e62353ef04b22c15c15537756c45714f4eda91
JohnnyAstrom <johnny.astrom@hotmail.com>
2024-11-11 20:31:23 +0100

    Changed nav-bar from a separate menu for mobile and desktop device into only one single menu for both devices.

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 const menuButton = document.querySelector('.header__mobile-menu-button');
-const closeMenuButton = document.querySelector('.header__mobile-close-menu');
-const menu = document.querySelector('.header__mobile-menu');
+const closeMenuButton = document.querySelector('.nav__mobile-close-menu');
+const menu = document.querySelector('.nav');
 const backgroundDimmed = document.querySelector(".header__mobile-menu-background-dimmed");
 
 menuButton.addEventListener('click', () => {
-    backgroundDimmed.classList.add("header__mobile-menu-background-dimmed-active");
-    setTimeout(() => {
-    menu.classList.add('header__mobile-menu-active');
-    }, 100);
-  });
-  
-  closeMenuButton.addEventListener('click', () => {
-    backgroundDimmed.classList.remove("header__mobile-menu-background-dimmed-active");
-    menu.classList.remove('header__mobile-menu-active');
-  });
+  backgroundDimmed.classList.add("header__mobile-menu-background-dimmed-active");
+  setTimeout(() => {
+    menu.classList.add('nav__mobile-menu-active');
+  }, 200);
+});
+
+closeMenuButton.addEventListener('click', () => {
+  backgroundDimmed.classList.remove("header__mobile-menu-background-dimmed-active");
+  menu.classList.remove('nav__mobile-menu-active');
+});
